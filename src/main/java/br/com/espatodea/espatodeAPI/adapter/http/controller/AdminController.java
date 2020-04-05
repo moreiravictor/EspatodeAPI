b191363e5ef1dbeec3483b1e95452e1dba9489bb
Victor Moreira <victormoreira176@hotmail.com>
2020-04-05 17:18:39 -0300

    changing adm request

--- src/main/java/br/com/espatodea/espatodeAPI/adapter/http/controller/AdminController.java
+++ src/main/java/br/com/espatodea/espatodeAPI/adapter/http/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.espatodea.espatodeAPI.core.model.Admin;
@@ -21,7 +22,7 @@ public class AdminController {
 	AdminService service = new AdminService();
 	
 	@GetMapping("/get")
-	public HttpReturn<Admin> userMatches(@RequestBody Admin admin) {
+	public HttpReturn<Admin> userMatches(@RequestParam Admin admin) {
 		return new HttpReturn<Admin>(service.getAdmin(admin), HttpStatus.FOUND);
 	}
 
