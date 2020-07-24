bdcbe49e46c12c839ef075a3ca818d689b8685fa
Victor Moreira <victormoreira176@hotmail.com>
2020-07-24 01:07:56 -0300

    improving readability

--- src/main/java/br/com/espatodea/espatodeAPI/adapter/http/controller/NewsletterController.java
+++ src/main/java/br/com/espatodea/espatodeAPI/adapter/http/controller/NewsletterController.java
@@ -1,43 +1,15 @@
 package br.com.espatodea.espatodeAPI.adapter.http.controller;
 
 import br.com.espatodea.espatodeAPI.core.model.HttpReturn;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.Base64;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import br.com.espatodea.espatodeAPI.core.service.NewsletterService;
 import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
 import com.google.api.services.gmail.model.Message;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.*;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
-
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 @CrossOrigin
@@ -45,81 +17,20 @@ import javax.mail.internet.MimeMessage;
 @RequestMapping("news")
 public class NewsletterController {
 
-    //GOOGLE API CONFIGS
-    private static final String APPLICATION_NAME = "espatodea";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private NewsletterService service = new NewsletterService();
+    private static final String source_email = "espatodea3@gmail.com";
 
-    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        InputStream in = NewsletterController.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("online")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("espatodea3@gmail.com");
+    @PostMapping("/auth")
+    public HttpReturn<Object> auth() throws IOException, GeneralSecurityException {
+        return service.auth();
     }
 
     @PostMapping
-    public HttpReturn<Object> auth() throws IOException, GeneralSecurityException {
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        // Print the labels in the user's account.
-        String user = "me";
-        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-        List<Label> labels = listResponse.getLabels();
-        if (labels.isEmpty()) {
-            return new HttpReturn<Object>(null, HttpStatus.NO_CONTENT);
-        } else {
-            return new HttpReturn<Object>(labels, HttpStatus.OK);
-        }
-    }
-
-    public static MimeMessage createEmail(String from, String to, String subject, String bodyText) throws MessagingException {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-
-        MimeMessage email = new MimeMessage(session);
-        email.setFrom(new InternetAddress(from));
-        email.addRecipient(javax.mail.Message.RecipientType.TO,
-                new InternetAddress(to));
-        email.setSubject(subject);
-        email.setText(bodyText);
-        return email;
-    }
-
-    @PostMapping("/email")
-    public HttpReturn<Object> sendEmail(@RequestParam List<String> emails) throws MessagingException, IOException, GeneralSecurityException {
-        MimeMessage email = createEmail("espatodea3@gmail.com", "ruivogamer17@gmail.com", "test", "test body");
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        email.writeTo(buffer);
-        byte[] bytes = buffer.toByteArray();
-        String encoded_email = Base64.encodeBase64URLSafeString(bytes);
-        Message message = new Message();
-        message.setRaw(encoded_email);
-
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        message = service.users().messages().send("espatodea3@gmail.com", message).execute();
-
-
+    public HttpReturn<Object> sendEmail(@RequestParam List<String> emails, @RequestBody String bodyText, @RequestParam String subject) throws MessagingException, IOException, GeneralSecurityException {
+        MimeMessage email = service.createEmail(source_email, emails, subject, bodyText);
+        Message message = service.createMessage(email);
+        Gmail gmailService = service.getGmailervice();
+        message = gmailService.users().messages().send(source_email, message).execute();
 
         return new HttpReturn<>(message, HttpStatus.OK);
     }
